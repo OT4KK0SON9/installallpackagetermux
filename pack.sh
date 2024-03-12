5918869e6b7d8e05e7cf2e31a1e728fda012105d
Kosong <160365589+OT4KK0SON9@users.noreply.github.com>
2024-03-12 07:06:09 +0700

    Add files via upload

---
+++ pack.sh
@@ -0,0 +1,30 @@
+echo Lapor Bug > +6283841852697
+echo Lapor Bug > +6283841852697
+echo Lapor Bug > +6283841852697
+echo Lapor Bug > +6283841852697
+pkg install figlet
+figlet Mulai Mengunduh
+pkg install tur-repo
+pkg install nano
+pkg install nmap
+pkg install nodejs-12
+pkg install nodejs-16
+pkg install nodejs-18
+pkg install openspecfun
+pkg install openssh
+pkg install openssl
+pkg install openvdb
+pkg install python3.10
+pkg install python3.7
+pkg install python3.8
+pkg install python3.8
+pkg install python3.9
+pkg install python3
+pkg install python
+pkg install ruby
+pkg install tar
+pkg install zphisher
+pkg autoclean
+echo Lapor Bug > +6283841852697
+echo Lapor Bug > +6283841852697
+echo Lapor Bug > +6283841852697
