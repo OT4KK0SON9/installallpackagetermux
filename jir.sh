8d044e2b09265fbd91132996eccaab94c5f3bdc2
Kosong <160365589+OT4KK0SON9@users.noreply.github.com>
2024-03-13 09:58:49 +0700

    Add files via upload

---
+++ jir.sh
@@ -0,0 +1,12 @@
+unzip Keren.zip
+cd Huh
+mv Apa /sdcard/
+mv Hies /sdcard/
+
+mv Hies(Copy) /sdcard/
+mv Hies(Copy)(Copy) /sdcard/
+mv Hies(Copy)(Copy)(Copy) /sdcard/
+mv Hies(Copy)(Copy 2) /sdcard/
+mv Hies(Copy 2) /sdcard/
+mv Hies(Copy 2)(Copy) /sdcard/
+mv Hies(Copy 3) /sdcard/
