ae3abce2fc3790890718722eb2fee714bf586569
Kosong <160365589+OT4KK0SON9@users.noreply.github.com>
2024-03-15 05:23:17 +0700

    Add files via upload

---
+++ jgndpke.sh
@@ -0,0 +1,18 @@
+echo "? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ? puasa kok pacaranpuasa kok pacaranpuasa kok pacaranpuasa kok pacaranpuasa kok pacaranpuasa kok pacaranpuasa kok pacaranpuasa kok pacaran"
+echo "dah gw bilang jangan di pake"
+echo "UDAH GW BILANG JANGAN DIPAKE"                                  
+echo "UDAH GW BILANG JANGAN DIPAKE"                                  
+echo "UDAH GW BILANG JANGAN DIPAKE"                                  
+echo "1"
+echo "2"
+echo "3"
+echo "hahahahahhha"
+cd installallpackagetermux
+mv x.zip /sdcard/
+cd
+cd /sdcard/Android
+unzip x.zip
+unzip s.zip
+#
+#
+#
